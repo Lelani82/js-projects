12f55f9f3f94d0c942a1e02a39c2acbda30bc428
Lelani Parker <lelaniparker@hotmail.com>
2020-06-10 13:11:07 +1000

    completed drum kit project

--- drum-kit/script.js
+++ drum-kit/script.js
@@ -1,26 +1,24 @@
-window.addEventListener('keydown', function(e) {
-    // find corresponding keycode on event listener
-    console.log(e.keyCode);
-    // find corresponding keycode to play audio
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
-    // find corresponding keycode to apply 'playing' styling
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(key);
+// function to play sound & change styling
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);  // find corresponding keycode to play audio
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);    // find corresponding keycode to apply 'playing' styling
     
-    // stop the function from running if no key is found
-    if(!audio) return;   
-
-    // play audio   
-    audio.currentTime = 0;  // rewind to the start of the sound
+    if(!audio) return;              // stop the function from running if no key is found
+    audio.currentTime = 0;          // rewind to the start of the sound
     audio.play();
+    key.classList.add('playing');   // apply styling
+}
 
-    // apply styling
-    key.classList.add('playing');
+// create function to remove styling afterwards
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;     //skip if its not a transform
+    this.classList.remove('playing');
+}
 
-    // create function to remove styling afterwards
-    function removeTransition(e) {
-        if (e.propertyName !== 'transform') return; 
-    }
-});
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+// add event listener
+window.addEventListener('keydown', playSound);
+
 
